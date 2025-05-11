f10074d71d9651671065e1d039d83ec061ca7c8f
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2025-05-11 19:37:27 +0300

    Update response.middleware.js

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,5 +1,20 @@
 const responseMiddleware = (req, res, next) => {
-  // TODO: Implement middleware that returns result of the query
+  const responseMiddleware = (req, res, next) => {
+    res.sendSuccess = (data) => {
+      res.status(200).json({ success: true, data });
+    };
+
+    res.sendBadRequest = (message) => {
+      res.status(400).json({ success: false, error: message });
+    };
+
+    res.sendNotFound = (message = "Not found") => {
+      res.status(404).json({ success: false, error: message });
+    };
+
+    next();
+  };
+
   next();
 };
 
